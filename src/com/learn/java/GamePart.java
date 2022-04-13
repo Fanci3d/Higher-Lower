d8dd32b9ab8b9d30b8680a196c3a96d6e8192b89
Fancyed <jaden.bossenbroek@gmail.com>
2022-04-13 14:55:43 -0400

    added number of guesses

--- src/com/learn/java/GamePart.java
+++ src/com/learn/java/GamePart.java
@@ -1,5 +1,6 @@
 package com.learn.java;
 
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,6 +11,7 @@ int randomNumber;
     public void mainGame(int higher, int lower)
     {
         int input;
+        int counterOfGuesses=0;
         Scanner getPlayerInput = new Scanner(System.in);
 
         this.randomNumber = getRandomNumber(higher, lower);
@@ -17,6 +19,8 @@ int randomNumber;
         do {
             System.out.println("Guess a Number");
             input = getPlayerInput.nextInt();
+            counterOfGuesses++;
+
             if(input > randomNumber)
                 System.out.println("The number is Lower");
             else if(input < randomNumber)
@@ -29,7 +33,7 @@ int randomNumber;
 
         System.out.println("You got the Number");
 
-
+        System.out.println("You got it in " + counterOfGuesses + " Trys");
     }
 
 
